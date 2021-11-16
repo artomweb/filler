756e4ed06f694e8ef5f6268b6f21c01c2b666c90
artom web <artomweb@gmail.com>
2021-11-16 19:50:51 +0000

    overlay hide order

--- js/main.js
+++ js/main.js
@@ -79,7 +79,6 @@ socket.on("gameUpdate", (data) => {
 
 socket.on("gameOver", (data) => {
     console.log(data);
-    overlayMsg.style.display = "none";
     if (data.status == "WIN") {
         createMessage("", data.message, "alert-success", false);
     } else if (data.status == "LOST") {
@@ -89,6 +88,7 @@ socket.on("gameOver", (data) => {
     }
     board = data.thisGame.board;
     gameStep();
+    overlayMsg.style.display = "none";
 });
 
 function createMessage(name, message, type, timed = true) {
